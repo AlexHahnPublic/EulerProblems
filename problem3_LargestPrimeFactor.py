e9a7d4353431338fd6a59815a4410b2b245d45a7
Alex Hahn <afh53@physics.cornell.edu>
2015-04-23 11:44:27 -0400

    took a good look at, edited the todo
    need to deal with when thge multiplicity of the largest prime divisor is greater than one

--- problem3_LargestPrimeFactor.py
+++ problem3_LargestPrimeFactor.py
@@ -8,7 +8,7 @@ def largestPrimeFactor(n):
    # This nex statement is often confused/ not stated correctly as "the largest
    # prime factor of a (composite) number is less than sqrt(n)". That's simply
    # not true, take a look at 14=7*2 and sqrt(14)=3 something. Really the
-   # statement is: for every composite number there exists a prime p s.t.
+   # statement is: for every composite number n there exists a prime p s.t.
    # p<sqrt(n). We can still leverage this fact! Many people just claim so for
    # the wrong reason. Basically we only need to check up to the sqrt(n)
    # integer divisors to tell whether the number n is prime or not (obviously can't be
@@ -21,8 +21,7 @@ def largestPrimeFactor(n):
    # has multiplicity greater than 1... really need to fix! The only thing I
    # can think of off the top of my head is to check the bool of a subfunction
    # that checks whether the modulo is an integer root of the number, if true
-   # then just exit the loop, if false continue into the inner loop. So
-   # basically take the negation of the subfunction
+   # then just exit the loop, if false continue into the inner loop
     while i * i <= n:
         print "inner loop, i is currently: ", i
         while n%i == 0:
@@ -30,7 +29,8 @@ def largestPrimeFactor(n):
             n /= i
             print "So divide n by ", i, "so n= ",n
         i = i + 1 # Someone once told me increment this by 2 coz "it's faster
-                  # in larger n", that spits in the face of twin primes lol
+                  # in larger n", this can only be done after 2 and 3 is 
+                  #completely factored out.. not sure worth the code change
         print "outside inner loop, increment i, i is now: ", i
     print n
 
