d7b09279dc52ef3818276325346f493ce47d7c0c
Alex Hahn <afh53@physics.cornell.edu>
2015-04-28 21:07:39 -0400

    Update problem1_3sand5s.py

--- problem1_3sand5s.py
+++ problem1_3sand5s.py
@@ -4,12 +4,20 @@
 
 # Find the sum of all the multiples of 3 and 5 below 1000.
 
+# Solution: trial division, check each number up to n, if divisible by 3 or 5
+# add it to the running total
+
+import time
+
 def sum3and5(n):
+    start_time = time.time()
     total = 0
     for nat in range(n):
         if (nat % 3 == 0) or (nat % 5 == 0):
             total = total + nat
-    print total
+    total_time = time.time() - start_time
+    print "The sum of all the multiples of 3 and 5 below", n, "is", total
+    print "This program took:", total_time, "seconds to run"
 
 if __name__ == "__main__":
     import sys
