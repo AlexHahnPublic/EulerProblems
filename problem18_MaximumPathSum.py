70291cf73a075812e815c86ea0c1af5e84a41e31
Alex Hahn <afh53@physics.cornell.edu>
2015-06-02 16:08:22 -0400

    Finished problem 18

--- problem18_MaximumPathSum.py
+++ problem18_MaximumPathSum.py
@@ -24,4 +24,28 @@
 #              91  71  52  38  17  14  91  43  58  50  27  29  48
 #            63  66  04  68  89  53  67  30  73  16  69  87  40  31
 #          04  62  98  27  23  09  70  98  73  93  38  53  60  04  23
+import time as T
 
+def maxPath():
+    start_time = T.time()
+    triangleFile = open('triangle.txt')
+    triangle = []
+
+    for i in triangleFile:
+        triangle.append([int(x) for x in i.strip().split() ])
+
+    for row in range(1,len(triangle)):
+        for col in range(len(triangle[row])):
+            if col == 0:
+                triangle[row][col] += triangle[row-1][col]
+            elif col == len(triangle[row-1]):
+                triangle[row][col] += triangle[row-1][col-1]
+            else:
+                triangle[row][col] += max(triangle[row-1][col],
+                        triangle[row-1][col-1])
+    total_time = T.time() - start_time
+    print "The sum of the path adding to the largest total from top row to bottom row is:", max(triangle[len(triangle)-1])
+    print "This program took", total_time,"seconds to run"
+
+if __name__ == "__main__":
+    maxPath()
