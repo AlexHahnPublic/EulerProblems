be98a1f1083b0af340f90e88ac55a5c4dcbdd092
MonsieurLilBakingSoda <afh53@physics.cornell.edu>
2016-07-27 13:57:29 -0700

    started probelm 53

---
+++ problem53_CombinatoricSelections.py
@@ -0,0 +1,10 @@
+# Euler Problem 53:
+# There are exactly ten ways of selecting three from five, 12345:
+#   123, 124, 125, 134, 135, 145, 234, 245, 345
+# In combinatorics, we use the notation 5C3 = 10
+# In general,
+#   nCr = (n!)/(r!(n-r)!), where r <= n, n! = n x (n-1) n ... x 2 x 1, and 0!=1
+#   by convention
+# It is not until n = 23, that a value exceeds one-million: 23 C 10 = 114406.
+#
+# How many, not necessarily distinct, values, of nCr, for 1
